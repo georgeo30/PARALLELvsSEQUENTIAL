d2e964bf43747adc8705d6c4b691a190e5f1a2e4
Georgeo <georgeosabu@gmail.com>
2019-08-26 18:41:28 +0200

    C3: A3 Vector class added with x and y variables

---
+++ ParallelVsSequential/src/Vector.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author George
+ */
+public class Vector {
+    public Float x;
+    public Float y;
+    
+    Vector(){
+        this.x=null;
+        this.y=null;
+    }
+}
